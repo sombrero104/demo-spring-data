5d78ed981b6268a233a617ca0f879ca5113cad62
sombrero104 <sombrero.khy@gmail.com>
2021-03-01 20:32:05 +0900

    .

---
+++ src/main/java/me/sombrero/demospringdata/DemoSpringDataApplication.java
@@ -0,0 +1,13 @@
+package me.sombrero.demospringdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringDataApplication.class, args);
+    }
+
+}
