59473e6ac60876e37d0373188cfc993bdb736318
sombrero104 <sombrero.khy@gmail.com>
2021-03-02 18:52:41 +0900

    .

--- src/main/java/me/sombrero/demospringdata/Address.java
+++ src/main/java/me/sombrero/demospringdata/Address.java
@@ -4,7 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
 /**
- * [ Value 타입 맵핑 ]
+ * [ Composite Value 타입 맵핑 ]
  * Address가 독립적으로 레퍼런스가 되어야 하는 경우에는 Address도 엔티티로 만들 수 있다.
  * 하지만 이 예제에서는 Value 타입으로 만들었다.
  * 이 예제에서는 Address를 Account에 속한 하나의 데이터로 취급한다.
@@ -13,6 +13,7 @@ import javax.persistence.Embeddable;
  * 이와 같이 어떠한 엔티티에 종속적인 타입을 Value 타입이라고 보면 된다.
  * Value 타입에는 @Embeddable 애노테이션을 붙인다.
  * 이 Address를 사용하는 엔티티쪽에서는 @Embedded를 붙여준다.
+ * Account 테이블이 만들어질 때 이 Address의 필드들도 Account 테이블 안에 컬럼이 만들어진다.
  */
 @Embeddable
 public class Address {
