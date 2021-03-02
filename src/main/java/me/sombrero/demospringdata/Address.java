d2bf42b0e6770358fcd3fde50a8f4fb7f8af6bbe
sombrero104 <sombrero.khy@gmail.com>
2021-03-02 18:47:23 +0900

    .

--- src/main/java/me/sombrero/demospringdata/Address.java
+++ src/main/java/me/sombrero/demospringdata/Address.java
@@ -1,5 +1,8 @@
 package me.sombrero.demospringdata;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
 /**
  * [ Value 타입 맵핑 ]
  * Address가 독립적으로 레퍼런스가 되어야 하는 경우에는 Address도 엔티티로 만들 수 있다.
@@ -8,6 +11,21 @@ package me.sombrero.demospringdata;
  * 때문에 이 Address의 생명주기는 Account 엔티티에 속해있다.
  * Account 엔티티가 만들어지면 Address도 만들어지고, Account 엔티티가 삭제되면 같이 삭제된다.
  * 이와 같이 어떠한 엔티티에 종속적인 타입을 Value 타입이라고 보면 된다.
+ * Value 타입에는 @Embeddable 애노테이션을 붙인다.
  */
+@Embeddable
 public class Address {
+
+    /**
+     * 이 곳의 필드들도 모두 @Column 이 생략되어 있다.
+     */
+    // @Column
+    private String street;
+
+    private String city;
+
+    private String state;
+
+    private String zipCode;
+
 }
