f220367dd858a978f8b73ad4ec1da6905222718d
sombrero104 <sombrero.khy@gmail.com>
2021-03-02 11:49:16 +0900

    .

--- src/main/java/me/sombrero/demospringdata/JpaRunner.java
+++ src/main/java/me/sombrero/demospringdata/JpaRunner.java
@@ -1,5 +1,6 @@
 package me.sombrero.demospringdata;
 
+import org.hibernate.Session;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -21,7 +22,13 @@ public class JpaRunner implements ApplicationRunner {
         account.setUsername("Anton");
         account.setPassword("pass");
 
-        entityManager.persist(account);
+        // entityManager.persist(account); // JPA로 저장하기.
+
+        /**
+         * 아래와 같이 하이버네이트의 API인 Session을 꺼내서 사용할 수도 있다.
+         */
+        Session session = entityManager.unwrap(Session.class);
+        session.save(account); // 하이버네이트로 저장하기.
     }
 
 }
