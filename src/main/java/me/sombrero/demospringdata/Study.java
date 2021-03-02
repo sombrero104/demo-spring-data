8d4251715d01be766e81688be3a835e76200e0f5
sombrero104 <sombrero.khy@gmail.com>
2021-03-02 21:47:13 +0900

    .

--- src/main/java/me/sombrero/demospringdata/Study.java
+++ src/main/java/me/sombrero/demospringdata/Study.java
@@ -21,7 +21,7 @@ public class Study {
      * 실행하면 Study 테이블에는 Account의 외래키인 owner_id가 만들어진다.
      * Study 엔티티에서 Account 엔티티와의 관계를 설정했으므로
      * Study가 이 관계에 대한 주인이며, Study가 이 관계를 관리한다.
-     */
+     */ 
     @ManyToOne // 한 사람이 여러개의 스터디를 만들 수 있다.
     private Account owner; // 스터디를 만든 사람.
 
