5edcac6392d1081dc20d8064562174ec36b8b5f6
sombrero104 <sombrero.khy@gmail.com>
2021-03-03 18:39:04 +0900

    .

--- src/main/java/me/sombrero/demospringdata/PostRepository.java
+++ src/main/java/me/sombrero/demospringdata/PostRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * 원래는 스프링부트가 아니라면 @Configuration이 붙은 클래스에 @EnableJpaRepositories를 붙여줘야 한다.
+ * 스프링부트에서는 자동으로 설정해준다.
+ * 스프링부트가 아니라면 main()메소드가 있는 @SpringBootApplication쪽에 붙여주면 된다.
+ * (@SpringBootApplication가 @Configuration이기 때문에..)
  */
 public interface PostRepository extends JpaRepository<Post, Long> { // < 엔티티 타입, 엔티티에서 사용하는 아이디(PK 타입) >
 }
