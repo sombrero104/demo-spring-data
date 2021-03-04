41c5844f4d68e36c86820c85e739f50e7aa3e853
sombrero104 <sombrero.khy@gmail.com>
2021-03-04 17:06:11 +0900

    .

---
+++ src/main/java/me/sombrero/demospringdata/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * package-info.java 파일을 만들고
+ * 이 파일의 패키지 위에 @NonNullApi를 붙이면
+ * 이 패키지 안의 모든 메서드의 리턴타입, 파라미터들이 다 @NonNull이 붙는 것과 마찬가지이다.
+ */
+// @org.springframework.lang.NonNullApi
+package me.sombrero.demospringdata;
